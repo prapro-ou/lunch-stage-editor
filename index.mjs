9a1cf0e6089ecde8dfce84791c2c531043d2ff7a
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-07-06 12:00:09 +0900

    refactor: stageを別ファイルに分けた

--- index.mjs
+++ index.mjs
@@ -1,30 +1,5 @@
-const obstacleType = {
-    mud: "obstacleType.mud"
-}
-
-let stage = {
-    roadPoint: [
-        {d: 0, x: 22}, // dは上方向(yとは逆の向き)
-        {d: 150, x: 22},
-        {d: 250, x: 42},
-        {d: 450, x: 42},
-        {d: 500, x: 52},
-        {d: 650, x: 52},
-        {d: 750, x: 72},
-        {d: 850, x: 72},
-        {d: 950, x: 52},
-        {d: 1050, x: 52},
-        {d: 1150, x: 32},
-        {d: 1350, x: 32},
-        {d: 1400, x: 22},
-        {d: 1500, x: 22}
-    ],
-    obstacles: [
-        // {type: obstacleType.mud, d: 120, x: 15},
-    ],
-    roadWidth: 40.0,
-    goalDistance: 1500
-}
+import { obstacleType } from "./enum.mjs";
+import { stage } from "./stage.mjs";
 
 const mudImage = new Image();
 mudImage.src = 'image/mud.png';
